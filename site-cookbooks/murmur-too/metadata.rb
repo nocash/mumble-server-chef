dc608aad5eefb0311c15a66dc2fff4ff6e6d6079
Beau Dacious <dacious.beau@gmail.com>
2014-07-13 23:13:12 -0700

    add TODOs

--- site-cookbooks/murmur-too/metadata.rb
+++ site-cookbooks/murmur-too/metadata.rb
@@ -7,6 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 # TODO: support adding of mumble apt repository
+# TODO: support creating config file from other cookbook
+# TODO: support user/group other than "mumble-server"
 
 depends 'apt'
 supports 'ubuntu'
