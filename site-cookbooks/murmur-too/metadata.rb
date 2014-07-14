5f32e91e93f1ba3fb8d204e80c6865147224ac76
Beau Dacious <dacious.beau@gmail.com>
2014-07-13 22:54:51 -0700

    update cookbook metadata

--- site-cookbooks/murmur-too/metadata.rb
+++ site-cookbooks/murmur-too/metadata.rb
@@ -2,6 +2,11 @@ name             'murmur-too'
 maintainer       'Beau Dacious'
 maintainer_email 'z727090@gmail.com'
 license          'MIT'
-description      'Installs/Configures murmur-too'
+description      'Installs/Configures murmur'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+# TODO: support adding of mumble apt repository
+
+depends 'apt'
+supports 'ubuntu'
