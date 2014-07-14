b44aa620702d3071aa0bd5469697084285accf09
Beau Dacious <dacious.beau@gmail.com>
2014-07-13 22:24:01 -0700

    generate cookbook

---
+++ site-cookbooks/murmur-too/metadata.rb
@@ -0,0 +1,7 @@
+name             'murmur-too'
+maintainer       'Beau Dacious'
+maintainer_email 'z727090@gmail.com'
+license          'MIT'
+description      'Installs/Configures murmur-too'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
