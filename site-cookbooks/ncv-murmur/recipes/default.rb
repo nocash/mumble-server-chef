3f2544f7cd0948a2cec7a0724087129127d1b1ce
Beau Dacious <dacious.beau@gmail.com>
2014-08-26 23:18:11 -0700

    handle encrypted data bag values in recipe

--- site-cookbooks/ncv-murmur/recipes/default.rb
+++ site-cookbooks/ncv-murmur/recipes/default.rb
@@ -36,7 +36,13 @@ template "#{ENV['HOME']}/.aws/credentials" do
   backup false
   source 'aws-credentials.erb'
 
-  aws_credentials = data_bag_item('aws-credentials', 'credentials')
+  secret_key_path =
+    "#{ENV['HOME']}/chef-credentials-secret-key"
+  secret_key =
+    Chef::EncryptedDataBagItem.load_secret(secret_key_path)
+  aws_credentials =
+    Chef::EncryptedDataBagItem.load('credentials', 'aws', secret_key)
+
   variables :credentials => {
     aws_access_key_id: aws_credentials['aws_access_key_id'],
     aws_secret_access_key: aws_credentials['aws_secret_access_key'],
