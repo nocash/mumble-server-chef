18e3080ea783f692640ef58e583ea72d78eb8bb5
Beau Dacious <dacious.beau@gmail.com>
2014-09-03 00:28:49 -0700

    fetch db backups from s3 on initial provision

--- site-cookbooks/ncv-server/recipes/backups.rb
+++ site-cookbooks/ncv-server/recipes/backups.rb
@@ -1,5 +1,20 @@
+include_recipe 'ncv-server::aws'
+
 include_recipe 'apt'
 package 'gzip'
+
+murmur_path = File.dirname(node[:murmur][:config][:database])
+s3_path = "s3://mumble-server/db-backups"
+
+backup_path = "#{murmur_path}/backups"
+
+bash 'initial_database_sync' do
+  code <<-EOF
+    mkdir -p #{backup_path}
+    aws s3 sync #{s3_path} #{backup_path}
+    EOF
+  creates "#{backup_path}/mumble-server.sqlite"
+end
 
 template '/etc/cron.daily/mumble-server-db-backup' do
   backup false
