bd7dfd02ef7021ae7183cdc37e87a6e9d41e2a4c
Beau Dacious <dacious.beau@gmail.com>
2014-09-13 10:33:32 -0700

    tweaks to murmur backup recipe

--- site-cookbooks/ncv-server/recipes/backups.rb
+++ site-cookbooks/ncv-server/recipes/backups.rb
@@ -3,6 +3,7 @@ include_recipe 'ncv-server::aws'
 include_recipe 'apt'
 package 'gzip'
 
+murmur_db_file = File.basename(node[:murmur][:config][:database])
 murmur_path = File.dirname(node[:murmur][:config][:database])
 s3_path = "s3://mumble-server/db-backups"
 
@@ -12,10 +13,10 @@ bash 'initial_database_sync' do
   code <<-EOF
     mkdir -p #{backup_path}
     aws s3 sync #{s3_path} #{backup_path}
-    rm -v #{murmur_path}/mumble-server.sqlite
-    cp -v #{backup_path}/mumble-server.sqlite #{murmur_path}/
-    EOF
-  creates "#{backup_path}/mumble-server.sqlite"
+    rm -v #{murmur_path}/#{murmur_db_file}
+    cp -v #{backup_path}/#{murmur_db_file} #{murmur_path}/
+  EOF
+  creates "#{backup_path}/#{murmur_db_file}"
 end
 
 template '/etc/cron.daily/mumble-server-db-backup' do
