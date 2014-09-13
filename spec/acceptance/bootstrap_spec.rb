d5ec773c8dbcbaec9b30ad18404df6fb2926b6ac
Beau Dacious <dacious.beau@gmail.com>
2014-09-13 09:30:58 -0700

    add more tests for murmur installation

--- spec/acceptance/bootstrap_spec.rb
+++ spec/acceptance/bootstrap_spec.rb
@@ -3,6 +3,7 @@ require "open3"
 
 describe "A newly provisioned server" do
   before :context do
+    next
     reinstall_cookbooks
     vm.destroy
     vm.boot
@@ -14,10 +15,42 @@ describe "A newly provisioned server" do
     expect(vm).to have_ssh
   end
 
-  it "has murmur installed" do
-    command = vm.run "service --status-all"
+  context "with murmur installed", :murmur do
+    it "is running the murmur service" do
+      command = vm.run "service --status-all 2>&1"
 
-    expect(command.output).to match "mumble-server"
+      expect(command.output).to match "mumble-server"
+    end
+
+    it "installed murmur to /var/lib" do
+      command = vm.run "ls /var/lib"
+
+      expect(command.output).to match "mumble-server"
+    end
+
+    it "created a database backup directory" do
+      command = vm.run "sudo ls /var/lib/mumble-server/backups"
+
+      expect(command.output)
+        .to match("mumble-server.sqlite")
+        .and match(/mumble-server\.\d{14}\.sqlite\.gz/)
+    end
+
+    it "has a database matching the most recent murmur backup" do
+      command = vm.run "sudo diff /var/lib/mumble-server/{,backups}/mumble-server.sqlite"
+
+      expect(command.output).to be_empty
+    end
+
+    it "runs a murmur database backup script daily" do
+      command = vm.run "ls /etc/cron.daily/mumble-server-db-backup"
+
+      expect(command.output).to eq "/etc/cron.daily/mumble-server-db-backup\n"
+    end
+  end
+
+  context "with AWS installed", :aws do
+    pending
   end
 
   def secret_key_file
@@ -33,8 +66,8 @@ describe "A newly provisioned server" do
     @virtual_machine ||= VirtualMachine.new
   end
 
-  ShellCommand = Struct.new(:command) do
-    attr_reader :output, :status
+  class ShellCommand < Struct.new(:command)
+    attr_reader :output, :error, :status
 
     def run
       exec prepare_command(command)
@@ -44,7 +77,7 @@ describe "A newly provisioned server" do
     private
 
     def exec(command)
-      @output, @status = Open3.capture2e(command)
+      @output, @error, @status = Open3.capture3(command)
     end
 
     def prepare_command(command)
